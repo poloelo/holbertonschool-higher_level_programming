d2019f81f9ae5760b5c835b388602a8520202108
poloelo <p.gioria2304@gmail.com>
2026-01-16 12:49:41 +0100

    rectif

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def discovr():
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
-
-
 if __name__ == "__main__":
-    discovr()
+    import hidden_4
+    
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
