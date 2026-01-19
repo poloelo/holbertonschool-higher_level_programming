0a826ffb114ef52b336d559fc616b399c5441a44
poloelo <p.gioria2304@gmail.com>
2026-01-19 11:14:53 +0100

    5th ex

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list): 
+        return my_list
+    new_list = []
+    for item in my_list:
+        new_list.append(item)
+    new_list[idx] = element
+    return new_list
