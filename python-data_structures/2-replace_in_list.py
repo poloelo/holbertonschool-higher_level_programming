9996468f1d4f8e9fe5f9a78c494a125f1afe31e1
poloelo <p.gioria2304@gmail.com>
2026-01-19 10:58:12 +0100

    rectif

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list): 
+        return None
+    for i in range(len(my_list)):
+        if i == idx:
+            my_list[i] = element
+            return my_list
