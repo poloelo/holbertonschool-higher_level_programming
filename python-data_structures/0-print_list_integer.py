8dc7894aeec4bd37fdf4dc5f7c7fa33fdb36da4c
poloelo <p.gioria2304@gmail.com>
2026-01-21 20:25:11 +0100

    rectif

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,3 +1,4 @@
 def print_list_integer(my_list=[]):
     for item in my_list:
         print(item)
+
---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    
+    fv_a, sv_a = tuple_a
+    fv_b, sv_b = tuple_b
+    n_tuple = (fv_a + fv_b, sv_a + sv_b)
+
+    
