7ad238c23df4552c78fbc9b9f650d21ab2309356
poloelo <p.gioria2304@gmail.com>
2026-02-12 11:06:34 +0100

    rectif

--- python-input_output /0-read_file.py
+++ python-input_output /0-read_file.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""Module that defines read_file function"""
+
+"""
+    Module to read file
+"""
 
 
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read(), end='')
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    Uses the with statement for file handling.
+    Does not handle file permission or file doesn't exist exceptions.
+
+    Args:
+    filename -- the name of the file to read (default is an empty string)
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
